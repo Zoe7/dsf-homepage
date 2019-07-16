fc52b806e37be286e05b0440fd3616e89b8ae94d
Zoé de Moffarts <zoe.demoffarts@gmail.com>
2019-07-16 16:09:23 +1000

    Add gatsby-plugin-styled-components for server side rendering support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,18 @@
 module.exports = {
   plugins: [
     {
-      resolve: 'gatsby-source-graphql',
+      resolve: "gatsby-source-graphql",
       options: {
         // This type will contain remote schema Query type
-        typeName: 'CMS',
+        typeName: "CMS",
         // This is field under which it's accessible
-        fieldName: 'cms',
+        fieldName: "cms",
         // Url to query from
         url:
-          'https://api-euwest.graphcms.com/v1/cjryto5wd3lza01gtcfgarpde/master'
+          "https://api-euwest.graphcms.com/v1/cjryto5wd3lza01gtcfgarpde/master"
       }
-    }
+    },
+    "gatsby-plugin-styled-components"
   ]
 };
 //https://api-euwest.graphcms.com/v1/cjryto5wd3lza01gtcfgarpde/master
